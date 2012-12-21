4119ff24810af45d6de48856665465ccc7057885
Jeremy Keeshin <jkeesh@gmail.com>
2012-12-21 02:40:48 -0800

    priority queue works as well

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 /// options = 'stack', 'queue'
-var DATA_STRUCTURE_TYPE = 'queue';
+var DATA_STRUCTURE_TYPE = 'pqueue';
+
+/*
+
+TODO: WHEN EMPTY GIVE MESSAGE
+
+*/
 
 
 /* 
@@ -139,6 +145,53 @@ FirebaseQueue.prototype.getDefaultTitle = function(){
 }
 
 
+//////////////////////////////////////////////
+// FirebasePriorityQueue
+//////////////////////////////////////////////
+
+/*
+A FirebaseQueue provides first-in-first-out (FIFO) access to a list
+When an item is added, it is added to the back of the list.
+*/
+FirebasePriorityQueue.prototype = new FirebaseDataStructure();
+FirebasePriorityQueue.prototype.constructor = FirebasePriorityQueue;
+
+function FirebasePriorityQueue(ref){
+	FirebaseDataStructure.call(this, ref);
+}
+
+FirebasePriorityQueue.prototype.getDefaultTitle = function(){
+	return "My Action PQueue";
+}
+
+/* 
+This function adds an item with value item to our priority queue.
+The only difference here is that we get the priority given to us
+explicitly.
+
+@param item 	{string}		the item to add 
+								must be of the form 
+
+									[priority] value
+
+								like
+
+									[5] clean up todos
+*/
+FirebasePriorityQueue.prototype.addOneItem = function(item){
+	var lbracket = item.indexOf('[')
+	var rbracket = item.indexOf(']');
+
+	var priorityString = item.substring(lbracket + 1, rbracket).trim();
+
+	var result = this.ref.push({
+		'value': item
+	});
+
+	result.setPriority(priorityString);
+}
+
+
 $(function(){
 	ActionStack = (function(type){
 		var ADD_BUTTON = '#add-button';
@@ -169,6 +222,9 @@ $(function(){
 			}else if(type == 'queue'){
 				var queueRef = pageRef.child('queue');
 				dataStructure = new FirebaseQueue(queueRef);
+			}else if(type == 'pqueue'){
+				var pqref = pageRef.child('pqueue');
+				dataStructure = new FirebasePriorityQueue(pqref);
 			}else{
 				console.log("Bad type");
 			}
